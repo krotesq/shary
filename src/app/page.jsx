09a7026cb1ff0d5521170362fef52baf9be3284b
aaronlyy <aaronlevican@gmail.com>
2023-09-25 18:57:28 +0200

    🐛 Fixed link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,9 +3,9 @@ import Link from "next/link"
 export default function Home() {
   return (
     <>
-      <Link href='/p/new'>Create a new paste</Link>
+      <Link href='/p'>Create a new paste</Link>
       <br />
-      <Link href='/s/'>Short an URL</Link>
+      <Link href='/s'>Short an URL</Link>
     </>
   )
 }
