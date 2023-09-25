d6fbd4dd0876e1220adbc0e375fd7d5671dfcb93
aaronlyy <aaronlevican@gmail.com>
2023-09-25 19:13:45 +0200

    ✨ Added font and css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,13 @@
+import Link from "next/link"
+import { Inter } from "next/font/google"
+import "./global.css"
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+})
+
 export const metadata = {
   title: 'shary | pastebin and url shortening',
   description: 'a pastebin and url shortening service',
@@ -5,9 +15,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable}`}>
       <body>
         <h1>shary: a pastebin & url shortening service</h1>
+        <Link href='/p'>Create a new paste</Link>
+        <br/>
+        <Link href='/s'>Short an URL</Link>
+        <hr />
         {children}
       </body>
     </html>
